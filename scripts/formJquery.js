8933520636801c09117c578b21bfea9a230c5a15
cesar-itacademy <cesar-itacademy@users.noreply.github.com>
2021-02-10 10:29:49 +0100

    Código formateado según instrucciones y añadido .gitignore

--- scripts/formJquery.js
+++ scripts/formJquery.js
@@ -1,55 +1,50 @@
 //SPRINT- 4 EJERCICIO DE VALIDACIÓN DE FORMULARIOS
 // LEVEL 3  JQUERY VALIDATION PLUGIN
 
-
 // =======================================
-//          JQUERY VALIDATION 
-// ===  Validacion de formulario Login ===       
+//          JQUERY VALIDATION
+// ===  Validacion de formulario Login ===
 // =======================================
 
 // JSON con las reglas de validación de los campos
 //=====================================================
 
 $("#loginForm").validate({
-    rules: {
-        loginEmail: {
-            required: true,
-            email: true
-        },
-        loginPassword: {
-            required: true,
-            minlength: 8
-        }
+  rules: {
+    loginEmail: {
+      required: true,
+      email: true,
     },
-    // Añadimos clase error al label para resaltar el error
-    highlight: function (element) {
-        $(element).parent().addClass('error')
+    loginPassword: {
+      required: true,
+      minlength: 8,
     },
-    unhighlight: function (element) {
-        $(element).parent().removeClass('error')
-    }
-}); 
-
+  },
+  // Añadimos clase error al label para resaltar el error
+  highlight: function (element) {
+    $(element).parent().addClass("error");
+  },
+  unhighlight: function (element) {
+    $(element).parent().removeClass("error");
+  },
+});
 
 // Validación del formulario con el evento click en enviar
 //========================================================
 
-$("#acceder").click( () => {
-    if ($("#loginForm").valid() == false) {
-        return;
-    }
-    let emailL    = $("#loginEmail").val();
-    let passwordL = $("#loginPassword").val();
+$("#acceder").click(() => {
+  if ($("#loginForm").valid() == false) {
+    return;
+  }
+  let emailL = $("#loginEmail").val();
+  let passwordL = $("#loginPassword").val();
 });
-
-
-
 
 /*
  * Translated default messages for the jQuery validation plugin.
  * Locale: ES
  */
- 
+
 jQuery.extend(jQuery.validator.messages, {
   required: "Este campo es obligatorio.",
   remote: "Por favor, rellena este campo.",
@@ -62,10 +57,22 @@ jQuery.extend(jQuery.validator.messages, {
   creditcard: "Por favor, escribe un número de tarjeta válido.",
   equalTo: "Por favor, escribe el mismo valor de nuevo.",
   accept: "Por favor, escribe un valor con una extensión aceptada.",
-  maxlength: jQuery.validator.format("Por favor, no escribas más de {0} caracteres."),
-  minlength: jQuery.validator.format("Por favor, no escribas menos de {0} caracteres."),
-  rangelength: jQuery.validator.format("Por favor, escribe un valor entre {0} y {1} caracteres."),
-  range: jQuery.validator.format("Por favor, escribe un valor entre {0} y {1}."),
-  max: jQuery.validator.format("Por favor, escribe un valor menor o igual a {0}."),
-  min: jQuery.validator.format("Por favor, escribe un valor mayor o igual a {0}.")
+  maxlength: jQuery.validator.format(
+    "Por favor, no escribas más de {0} caracteres."
+  ),
+  minlength: jQuery.validator.format(
+    "Por favor, no escribas menos de {0} caracteres."
+  ),
+  rangelength: jQuery.validator.format(
+    "Por favor, escribe un valor entre {0} y {1} caracteres."
+  ),
+  range: jQuery.validator.format(
+    "Por favor, escribe un valor entre {0} y {1}."
+  ),
+  max: jQuery.validator.format(
+    "Por favor, escribe un valor menor o igual a {0}."
+  ),
+  min: jQuery.validator.format(
+    "Por favor, escribe un valor mayor o igual a {0}."
+  ),
 });
